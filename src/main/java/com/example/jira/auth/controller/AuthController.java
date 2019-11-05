a8ecc64a2d889e22ddd2b6b302388b0101080044
Samba SG <sougousambaofficiel@gmail.com>
2019-11-05 14:35:39 +0000

    update password endpoint configured without proper error  handling

--- src/main/java/com/example/jira/auth/controller/AuthController.java
+++ src/main/java/com/example/jira/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.jira.auth.controller;
 
 import com.example.jira.auth.exception.AppException;
+import com.example.jira.auth.exception.InvalidOldPasswordException;
 import com.example.jira.auth.model.Role;
 import com.example.jira.auth.model.RoleName;
 import com.example.jira.auth.model.User;
@@ -11,10 +12,12 @@ import com.example.jira.auth.payload.SignUpRequest;
 import com.example.jira.auth.repository.RoleRepository;
 import com.example.jira.auth.repository.UserRepository;
 import com.example.jira.auth.security.JwtTokenProvider;
+import com.example.jira.auth.utils.GenericResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,16 +25,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.Collections;
+import java.util.Locale;
+import java.util.Optional;
 
 @Api( description="API pour la gestion des authentifications.")
 @RestController
@@ -41,6 +44,9 @@ public class AuthController {
 
     @Autowired
     AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MessageSource messages;
 
     @Autowired
     UserRepository userRepository;
@@ -135,5 +141,38 @@ public class AuthController {
                 .buildAndExpand(result.getUsername()).toUri();
 
         return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+    }
+
+    // NEED TO BE MOVED IN USER SERVICE
+    public void changeUserPassword(final User user, final String password) {
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+    }
+    public boolean checkIfValidOldPassword(final User user, final String oldPassword) {
+        return passwordEncoder.matches(oldPassword, user.getPassword());
+    }
+    @RequestMapping(value = "/updatePassword", method = RequestMethod.POST)
+    @ResponseBody
+    public GenericResponse changeUserPassword(Locale locale,
+                                              @RequestParam("password") String password,
+                                              @RequestParam("oldpassword") String oldPassword) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = "";
+        if (principal instanceof UserDetails) {
+             username = ((UserDetails)principal).getUsername();
+        } else {
+             username = principal.toString();
+        }
+
+// getAuthorities() - Returns the authorities granted to the user.
+//        System.out.println("User has authorities: " + userDetails.getAuthorities());
+        Optional<User> user = userRepository.findByUsername( username );
+
+        if (!checkIfValidOldPassword(user.get(), oldPassword)) {
+            throw new InvalidOldPasswordException();
+        }
+        changeUserPassword(user.get(), password);
+        return new GenericResponse("Mot de passe mis à jours avec succès");
     }
 }
